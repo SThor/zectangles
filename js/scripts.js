c21bb377cabcde339305f72b69f83696a74d3555
SThor <paulgivel@gmail.com>
2018-07-22 16:10:00 +0200

    added framing

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var canvas = {
   ctx:null,
   color_bg:"white",
   color_main:"black",
+  padding:50,
   settings:{
     cellsInWidth : 4,
     itInCell : 100,
@@ -14,8 +15,8 @@ var canvas = {
     
     var minSide = Math.min(window.innerWidth,window.innerHeight)
     
-    canvas.canvas.width = (2/3)*minSide;
-    canvas.canvas.height = (2/3)*minSide;
+    canvas.canvas.width = (4/5)*minSide;
+    canvas.canvas.height = (4/5)*minSide;
     
     /**
      * Your drawings need to be inside this function otherwise they will be reset when
@@ -27,22 +28,24 @@ var canvas = {
   drawStuff: function() {
     canvas.ctx.setTransform(1, 0, 0, 1, 0, 0);
     canvas.ctx.clearRect(0,0,canvas.canvas.width,canvas.canvas.height);
+    canvas.ctx.fillStyle = canvas.color_bg;
+    canvas.ctx.fillRect(0,0,canvas.canvas.width,canvas.canvas.height);
     canvas.ctx.strokeStyle = canvas.color_main;
-    canvas.ctx.translate(0.5,0.5);
-    canvas.ctx.strokeRect(0,0,canvas.canvas.width-1,canvas.canvas.height-1);
+    canvas.ctx.translate(0.5+canvas.padding,0.5+canvas.padding);
+    //canvas.ctx.strokeRect(0,0,canvas.canvas.width-1-2*canvas.padding,canvas.canvas.height-1-2*canvas.padding);
     
-    var cellLenght = (canvas.canvas.width-1)/canvas.settings.cellsInWidth;
+    var cellLenght = (canvas.canvas.width-2*canvas.padding-1)/canvas.settings.cellsInWidth;
     var i,j;
     for(i=0;i<canvas.settings.cellsInWidth;i++){
       for(j=0;j<canvas.settings.cellsInWidth;j++){
-        canvas.ctx.setTransform(1,0,0,1,(i*cellLenght)+cellLenght/2+0.5,(j*cellLenght)+cellLenght/2+0.5);
+        canvas.ctx.setTransform(1,0,0,1,(i*cellLenght)+cellLenght/2+0.5+canvas.padding,(j*cellLenght)+cellLenght/2+0.5+canvas.padding);
         canvas.drawCell(i,j,cellLenght);
       }
     }
   },
   
   drawCell: function(i,j,cellLenght){
-    canvas.ctx.strokeRect(0-cellLenght/2,0-cellLenght/2,cellLenght,cellLenght);
+    //canvas.ctx.strokeRect(0-cellLenght/2+canvas.padding,0-cellLenght/2+canvas.padding,cellLenght,cellLenght);
     var k;
     var angle = Math.atan(canvas.settings.offsetInIt/(1-canvas.settings.offsetInIt));
     var scale = canvas.settings.offsetInIt/Math.sin(angle);
